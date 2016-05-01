ccbc184a8776f8a1e2a2a2cc707241488f9d4596
zhantong1994 <zhantong1994@163.com>
2016-05-01 09:16:24 +0800

    增加诸多相机选项

--- app/src/main/java/com/polarxiong/camerademo/SettingsFragment.java
+++ app/src/main/java/com/polarxiong/camerademo/SettingsFragment.java
@@ -17,14 +17,20 @@ public class SettingsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
 
-        loadPreviewSize(mCamera);
+        loadSupportedPreviewSize(mCamera);
+        loadSupportedPictureSize(mCamera);
+        loadSupportedVideoeSize(mCamera);
+        loadSupportedFlashMode(mCamera);
+        loadSupportedFocusMode(mCamera);
+        loadSupportedWhiteBalance(mCamera);
+        loadSupportedSceneMode(mCamera);
     }
     static SettingsFragment newInstance(Camera camera){
         SettingsFragment settingsFragment=new SettingsFragment();
         mCamera=camera;
         return settingsFragment;
     }
-    private void  loadPreviewSize(Camera camera){
+    private void  loadSupportedPreviewSize(Camera camera){
         List<Camera.Size> listSupportedPreviewSize=camera.getParameters().getSupportedPreviewSizes();
         List<String> listEntries=new ArrayList<>();
         for(Camera.Size size:listSupportedPreviewSize){
@@ -33,6 +39,58 @@ public class SettingsFragment extends PreferenceFragment {
         }
         final CharSequence[] supportedPreviewSize=listEntries.toArray(new CharSequence[listEntries.size()]);
         ListPreference prefPreviewSize=(ListPreference)getPreferenceScreen().findPreference("preview_size");
+        prefPreviewSize.setEntries(supportedPreviewSize);
+        prefPreviewSize.setEntryValues(supportedPreviewSize);
+    }
+    private void  loadSupportedPictureSize(Camera camera){
+        List<Camera.Size> listSupportedPreviewSize=camera.getParameters().getSupportedPictureSizes();
+        List<String> listEntries=new ArrayList<>();
+        for(Camera.Size size:listSupportedPreviewSize){
+            String stringSize=size.width+"x"+size.height;
+            listEntries.add(stringSize);
+        }
+        final CharSequence[] supportedPreviewSize=listEntries.toArray(new CharSequence[listEntries.size()]);
+        ListPreference prefPreviewSize=(ListPreference)getPreferenceScreen().findPreference("picture_size");
+        prefPreviewSize.setEntries(supportedPreviewSize);
+        prefPreviewSize.setEntryValues(supportedPreviewSize);
+    }
+    private void  loadSupportedVideoeSize(Camera camera){
+        List<Camera.Size> listSupportedPreviewSize=camera.getParameters().getSupportedVideoSizes();
+        List<String> listEntries=new ArrayList<>();
+        for(Camera.Size size:listSupportedPreviewSize){
+            String stringSize=size.width+"x"+size.height;
+            listEntries.add(stringSize);
+        }
+        final CharSequence[] supportedPreviewSize=listEntries.toArray(new CharSequence[listEntries.size()]);
+        ListPreference prefPreviewSize=(ListPreference)getPreferenceScreen().findPreference("video_size");
+        prefPreviewSize.setEntries(supportedPreviewSize);
+        prefPreviewSize.setEntryValues(supportedPreviewSize);
+    }
+    private void  loadSupportedFlashMode(Camera camera){
+        List<String> listSupportedPreviewSize=camera.getParameters().getSupportedFlashModes();
+        final CharSequence[] supportedPreviewSize=listSupportedPreviewSize.toArray(new CharSequence[listSupportedPreviewSize.size()]);
+        ListPreference prefPreviewSize=(ListPreference)getPreferenceScreen().findPreference("flash_mode");
+        prefPreviewSize.setEntries(supportedPreviewSize);
+        prefPreviewSize.setEntryValues(supportedPreviewSize);
+    }
+    private void  loadSupportedFocusMode(Camera camera){
+        List<String> listSupportedPreviewSize=camera.getParameters().getSupportedFocusModes();
+        final CharSequence[] supportedPreviewSize=listSupportedPreviewSize.toArray(new CharSequence[listSupportedPreviewSize.size()]);
+        ListPreference prefPreviewSize=(ListPreference)getPreferenceScreen().findPreference("focus_mode");
+        prefPreviewSize.setEntries(supportedPreviewSize);
+        prefPreviewSize.setEntryValues(supportedPreviewSize);
+    }
+    private void  loadSupportedWhiteBalance(Camera camera){
+        List<String> listSupportedPreviewSize=camera.getParameters().getSupportedWhiteBalance();
+        final CharSequence[] supportedPreviewSize=listSupportedPreviewSize.toArray(new CharSequence[listSupportedPreviewSize.size()]);
+        ListPreference prefPreviewSize=(ListPreference)getPreferenceScreen().findPreference("white_balance");
+        prefPreviewSize.setEntries(supportedPreviewSize);
+        prefPreviewSize.setEntryValues(supportedPreviewSize);
+    }
+    private void  loadSupportedSceneMode(Camera camera){
+        List<String> listSupportedPreviewSize=camera.getParameters().getSupportedSceneModes();
+        final CharSequence[] supportedPreviewSize=listSupportedPreviewSize.toArray(new CharSequence[listSupportedPreviewSize.size()]);
+        ListPreference prefPreviewSize=(ListPreference)getPreferenceScreen().findPreference("scene_mode");
         prefPreviewSize.setEntries(supportedPreviewSize);
         prefPreviewSize.setEntryValues(supportedPreviewSize);
     }
